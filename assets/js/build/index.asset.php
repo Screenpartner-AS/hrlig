196b3a6451cf9d3df45730d769d05eb137d30663
michaelwilhelmsen <michael@screenpartner.no>
2025-05-01 16:24:04 +0200

    some styling for chat

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '4d5e4dd960c8b0a184f2');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '252159f3ccb7de2559d8');
