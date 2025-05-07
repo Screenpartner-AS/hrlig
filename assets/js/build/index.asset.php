2e9b73c92b996e5674f0f7c78994972427421831
michaelwilhelmsen <michael@screenpartner.no>
2025-05-07 12:19:10 +0200

    Some style changes

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '781ba7fe45b272965622');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '2be89a84821b4e41178f');
